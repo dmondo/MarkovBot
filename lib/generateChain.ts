125ba2f1d7fef3d7aa8c896bd6ce0df00df440df
dmondo <dannysmondo@gmail.com>
2020-06-03 22:07:37 -0700

    fix(generateChain): chains can now be arbitrary lengths

--- lib/generateChain.ts
+++ lib/generateChain.ts
@@ -1,14 +1,17 @@
-const generateChain = (model: any, chainLength: number): string => {
+const generateChain = (model: any, chainLength: number, n: number): string => {
   let chain = '';
   const lng = Object.keys(model).length;
   let next = Object.keys(model)[Math.floor(Math.random() * lng)];
+
   chain += next;
   for (let i = 0; i < chainLength; i += 1) {
-    if (!(next in model)) { break; }
+    if (!(next in model)) {
+      break;
+    }
     const following = model[next][Math.floor(Math.random() * model[next].length)];
     chain = `${chain} ${following}`;
     const wdCount = chain.split(' ').length;
-    next = `${chain.split(' ').slice(wdCount - chainLength + 1, wdCount).join(' ')} ${following}`;
+    next = `${chain.split(' ').slice(wdCount - n, wdCount).join(' ')}`;
   }
   return chain;
 };
