d746f2421e65b469b5bb311812dc85a7a612a697
dmondo <dannysmondo@gmail.com>
2020-06-03 21:09:52 -0700

    feat(app): we check db for model before making req to twitter to build it

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -18,6 +18,17 @@ const App = (): JSX.Element => {
   const [model, setModel] = useState<IModel>({});
 
   const getTweets = async (query: string): Promise<void> => {
+    const fetchModel = await fetch(`/models/${query}`);
+    const existingModel = await fetchModel.json();
+
+    if (existingModel.length) {
+      const rawModel = existingModel[0].model;
+      const parsedModel = JSON.parse(rawModel);
+      setModel(parsedModel);
+      return;
+    }
+
+    // model for user doesn't exist, fetch tweets and build
     let tweetArray = [];
     const res = await fetch(`/twitter/${query}`);
     const twts = await res.json();
@@ -51,11 +62,21 @@ const App = (): JSX.Element => {
       tweetArray = [...tweetArray, ...nmoreTweets.slice(1)];
     }
 
+    // todo: tweetArray should actually be markov generated
+    // tweets, NOT tweets from twitter api...
     setTweets(tweetArray);
 
     const tweetModel: IModel = buildModel(tweetArray, 3);
 
     setModel(tweetModel);
+
+    const url = `/models/${query}`;
+    const method = 'POST';
+    const headers = { 'Content-Type': 'application/json' };
+    const body = JSON.stringify(tweetModel);
+    const options = { method, headers, body };
+
+    fetch(url, options);
   };
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
@@ -63,9 +84,11 @@ const App = (): JSX.Element => {
     getTweets(user);
   };
 
+  // TODO: only render makeTweet button after model is generated
   const makeTweet = () => {
+    console.log('model', model);
     const chain = generateChain(model, 15);
-    console.log('chain', chain);
+    console.log('chain:', chain);
   };
 
   return (
