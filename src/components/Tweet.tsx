4e357913083f741d0a1ab555d33426474e6fb3d7
dmondo <dannysmondo@gmail.com>
2020-06-05 00:20:26 -0700

    feat(tweet) tweet component displays tweet card

---
+++ src/components/Tweet.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+    maxWidth: 500,
+    marginLeft: 5,
+    backgroundColor: 'rgb(21, 32, 43)',
+    color: 'white',
+    borderColor: 'rgb(29, 161, 242)',
+    flexGrow: 1,
+  },
+  title: {
+    fontSize: 14,
+    color: 'white',
+  },
+  pos: {
+    marginBottom: 0,
+    color: 'white',
+  },
+  btn: {
+    marginTop: 0,
+    color: 'white',
+  },
+});
+
+const Tweet = ({ twt }): JSX.Element => {
+  const classes = useStyles();
+  return (
+    <>
+      <Card className={classes.root} variant="outlined">
+        <CardContent>
+          <Typography variant="body2" component="p">
+            {twt.text}
+          </Typography>
+          <Typography className={classes.pos} color="textSecondary">
+            {twt.user}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button className={classes.btn} size="small">like</Button>
+        </CardActions>
+      </Card>
+    </>
+  );
+};
+
+Tweet.propTypes = {
+  twt: PropTypes.shape({
+    user: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default Tweet;
