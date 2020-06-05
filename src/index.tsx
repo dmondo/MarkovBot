dfb277674f836a5d5d12f25405f948940bf6f091
dmondo <dannysmondo@gmail.com>
2020-06-04 21:47:10 -0700

    feat(app): react context skeleton

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import './styles/App.css';
 
+const Store = React.createContext('');
+
+const test = 'testval';
+
 ReactDOM.render(
-  <App />,
+  <Store.Provider value={test}>
+    <App />
+  </Store.Provider>,
   document.getElementById('twitter-app'),
 );
