b8fac5a177b7bb1d38e764db81f5e1b0831d8675
dmondo <dannysmondo@gmail.com>
2020-06-05 16:57:01 -0700

    refactor(model): model building logic now resides entirely in lib folder

---
+++ lib/TweetToModel.ts
@@ -0,0 +1,38 @@
+const TweetToModel = async (query: string): Promise<IMKVTweet[]> => {
+  let tweetArray = [];
+  const res = await fetch(`/twitter/${query}`);
+  const twts = await res.json();
+
+  const moreTweets = twts.map((twt: any) => (
+    {
+      user: twt.user.name,
+      text: twt.text,
+      id: twt.id_str,
+    }
+  ));
+
+  tweetArray = [...tweetArray, ...moreTweets];
+
+  for (let i = 0; i < 18; i += 1) {
+    const maxId = tweetArray[tweetArray.length - 1].id;
+
+    // ignore linter because we need blocking between iterations
+    /* eslint no-await-in-loop: 0 */
+    const nres = await fetch(`/twitter/${query}/${maxId}`);
+    const ntwts = await nres.json();
+
+    const nmoreTweets = ntwts.map((twt: any) => (
+      {
+        user: twt.user.name,
+        text: twt.text,
+        id: twt.id_str,
+      }
+    ));
+
+    tweetArray = [...tweetArray, ...nmoreTweets.slice(1)];
+  }
+
+  return tweetArray;
+};
+
+export default TweetToModel;
