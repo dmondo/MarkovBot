9d67bf650ae681f8e5ae92d5191707298de41555
dmondo <dannysmondo@gmail.com>
2020-06-05 00:19:24 -0700

    feat(feed): feed component displays tweets

---
+++ src/components/Feed.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Store } from '../store/Store';
+import Tweet from './Tweet';
+
+// TODO better key for tweets, maybe mongoose id?
+const Feed = (): JSX.Element => {
+  const { state } = React.useContext(Store);
+  const { tweets } = state;
+  return (
+    <>
+      <section>
+        {
+          tweets.map((twt) => (
+            <Tweet twt={twt} key={twt.text} />
+          ))
+        }
+      </section>
+    </>
+  );
+};
+
+export default Feed;
