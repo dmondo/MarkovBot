3bc8caa3029f5dd53b17728f218ef7cb4ea8fc88
dmondo <dannysmondo@gmail.com>
2020-06-05 20:01:12 -0700

    init(emotion): loader for loading spinner

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-env', '@babel/react-env'],
+            plugins: ['emotion'],
           },
         },
       },
