f373f1965ab701b7f10be7696fa251bd1c37bde9
dmondo <dannysmondo@gmail.com>
2020-09-18 22:36:43 -0700

    feat(tests): loader component tests

--- tests/loader.test.js
+++ tests/loader.test.js
@@ -1,13 +1,18 @@
+import React from 'react';
+import Adapter from 'enzyme-adapter-react-16';
+import { configure, mount } from 'enzyme';
+import Loader from '../dist/src/components/Loader';
+import { StoreProvider } from '../dist/src/store/Store';
+
+configure({ adapter: new Adapter() });
+
 describe('loader component', () => {
-  it('should render', () => {
-
-  });
-
-  it('should return a div element', () => {
-
-  });
-
-  it('should only render while LOADING state is true', () => {
-
+  it('should render correctly', () => {
+    const wrapper = mount(
+      <StoreProvider>
+        <Loader />
+      </StoreProvider>,
+    );
+    wrapper.unmount();
   });
 });
