8ccdbfeb1dd34dec4e7f1f241466d1215ccac3a3
dmondo <dannysmondo@gmail.com>
2020-09-18 19:02:18 -0700

    feat(tests): feed component tests

--- tests/feed.test.js
+++ tests/feed.test.js
@@ -1,9 +1,19 @@
+import React from 'react';
+import Adapter from 'enzyme-adapter-react-16';
+import { configure, mount } from 'enzyme';
+import Feed from '../dist/src/components/Feed';
+import { StoreProvider } from '../dist/src/store/Store';
+
+configure({ adapter: new Adapter() });
+
 describe('feed component', () => {
-  it('should render', () => {
-
-  });
-
-  it('should return a div element', () => {
-
+  it('should render a section element', () => {
+    const wrapper = mount(
+      <StoreProvider>
+        <Feed />
+      </StoreProvider>,
+    );
+    expect(wrapper.find('section')).toHaveLength(1);
+    wrapper.unmount();
   });
 });
