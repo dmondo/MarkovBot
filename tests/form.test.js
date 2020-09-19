0b80dc6b90a57572641dfcd841503408a52b4152
dmondo <dannysmondo@gmail.com>
2020-09-18 19:33:26 -0700

    feat(tests) form component tests

--- tests/form.test.js
+++ tests/form.test.js
@@ -1,25 +1,20 @@
+import React from 'react';
+import Adapter from 'enzyme-adapter-react-16';
+import { configure, mount } from 'enzyme';
+import Form from '../dist/src/components/Form';
+import { StoreProvider } from '../dist/src/store/Store';
+
+configure({ adapter: new Adapter() });
+
 describe('form component', () => {
-  it('should render', () => {
-
-  });
-
-  it('should return a div element', () => {
-
-  });
-
-  it('should make GET req to twitter API on submit', () => {
-
-  });
-
-  it('should update MODELS in state on submit', () => {
-
-  });
-
-  it('should insert model in database on submit', () => {
-
-  });
-
-  it('should render loading spinner while calculating model', () => {
-
+  it('should render with an h1 tag and an input form', () => {
+    const wrapper = mount(
+      <StoreProvider>
+        <Form />
+      </StoreProvider>,
+    );
+    expect(wrapper.find('h1')).toHaveLength(1);
+    expect(wrapper.find('form')).toHaveLength(1);
+    wrapper.unmount();
   });
 });
