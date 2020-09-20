aa948ea0b7fb471ecf4462afcf163452ae73834f
dmondo <dannysmondo@gmail.com>
2020-09-19 20:02:43 -0700

    feat(tests): markov library tests

--- tests/markov.test.js
+++ tests/markov.test.js
@@ -1,9 +1,29 @@
+import buildModel from '../dist/lib/markovModel';
+import generateChain from '../dist/lib/generateChain';
+
 describe('Markov model', () => {
-  it('building a model creats a dictionary whose keys are n-grams', () => {
-
-  });
-
-  it('calling makeChain creates a markov chain of length <=L', () => {
-
+  it('building a model creates a dictionary whose keys are n-grams', () => {
+    const fakeTweets = [
+      {
+        user: 'a',
+        text: 'one two three four',
+        id: 1,
+        uuid: '1',
+      },
+      {
+        user: 'a',
+        text: 'two three four two three four two three four two three four',
+        id: 1,
+        uuid: '1',
+      },
+      {
+        user: 'a',
+        text: 'four five six four five six four five six four five six',
+        id: 1,
+        uuid: '1',
+      },
+    ];
+    const model = buildModel(fakeTweets, 3);
+    expect(model['one two three'][0]).toEqual('four');
   });
 });
