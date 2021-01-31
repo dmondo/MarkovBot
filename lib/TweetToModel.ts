ca206896add3fe2021976a3bb684380da0e02dd1
dmondo <dannysmondo@gmail.com>
2021-01-30 16:45:29 -0800

    feat(tweetModel): await/pagination for twitter api route

--- lib/TweetToModel.ts
+++ lib/TweetToModel.ts
@@ -1,32 +1,21 @@
 const TweetToModel = async (query: string): Promise<string[]> => {
+  let allTweets = [];
   let currentPage = 0;
-  const response = fetch(`/twitter/${query}`);
-  let responses = [response];
 
-  // check: what happens to pagination if we go past max number of tweets for account...
-  // is array empty (good) or do we get same batch of tweets (bad)
   for (let i = 0; i < 15; i += 1) {
-    currentPage += 200;
-    const nextResponse = fetch(`/twitter/${query}/${currentPage}`);
-    responses = [...responses, nextResponse];
+    const response = currentPage ? await fetch(`/twitter/${query}/${currentPage}`) : await fetch(`/twitter/${query}`);
+    const tweetObjects = await response.json();
+
+    const tweets = tweetObjects.map((tweet: any) => (
+      tweet.text
+    ));
+    allTweets = [...allTweets, ...tweets];
+
+    const [lastTweet] = tweetObjects.slice(-1);
+    currentPage = lastTweet.id;
   }
 
-  const responseData = await Promise.all(responses);
-  const responseJSON = await Promise.all(responseData.map((resData) => (
-    resData.json()
-  )));
-
-  const tweetObjects = [];
-  responseJSON.forEach((resJSON) => {
-    resJSON.forEach((tweetObject: any) => {
-      tweetObjects.push(tweetObject);
-    });
-  });
-
-  const tweets = tweetObjects.map((tweet: any) => (
-    tweet.text
-  ));
-  return tweets;
+  return allTweets;
 };
 
 export default TweetToModel;
