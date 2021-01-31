dd348d001404d086b7f9b29e09d3193b3cac3f9f
dmondo <dannysmondo@gmail.com>
2021-01-30 16:43:47 -0800

    init(eslint): update await rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,6 +27,7 @@ module.exports = {
     "import/extensions": "off",
     "no-unused-vars": "off",
     "@typescript-eslint/no-unused-vars": "error",
+    "no-await-in-loop": "off"
   },
   settings: {
   },
